05e1fcf63c19c10c75ce1fde3955725ea6d62f24
baekjungkim <zvgandam@gmail.com>
2022-08-15 10:34:05 +0900

    server init

---
+++ src/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+const handleListen = () => console.log(`Listening on http://localhost:${PORT}`);
+
+app.listen(PORT, handleListen);
