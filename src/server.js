435bc3daaff2add968beff87b699bd6c7ecf70b0
Kim Baekjung <zvgandam@gmail.com>
2022-08-16 18:15:56 +0900

    Chat completed

--- src/server.js
+++ src/server.js
@@ -17,11 +17,14 @@ const handleListen = () => console.log(`Listening on http://localhost:${PORT}`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
+  sockets.push(socket);
   console.log("Connected to browser ✅");
   socket.on("close", () => console.log("Disconnected from the browser ❌"));
   socket.on("message", (message) => {
-    console.log(message.toString());
+    sockets.forEach((aSocket) => aSocket.send(message.toString()));
   });
   socket.send("welcom noom!");
 });
