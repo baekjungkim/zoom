74d3843bf40ef4694ca594a03ac41cca6b0f028a
Kim Baekjung <zvgandam@gmail.com>
2022-08-17 16:10:07 +0900

    Rooms

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,15 +2,25 @@ const socket = io();
 
 const enter = document.getElementById("enter");
 const enterForm = enter.querySelector("form");
+const room = document.getElementById("room");
+const roomForm = room.querySelector("form");
 
-function backendDone(message) {
-  console.log(message);
+room.hidden = true;
+
+let roomName;
+
+function showRoom() {
+  enter.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName}`;
 }
 
 function handleRoomSubmit(event) {
   event.preventDefault();
   const input = enterForm.querySelector("input");
-  socket.emit("enter_room", { payload: input.value }, backendDone);
+  socket.emit("enter_room", input.value, showRoom);
+  roomName = input.value;
   input.value = "";
 }
 
